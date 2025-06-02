67083d970e629cfa4a3eb8b2794d0d663889199a
StarostIN2000 <72708700+StarostIN2000@users.noreply.github.com>
2025-06-02 19:29:04 +0300

    camera positions

--- Assets/Scripts/Controller/PlayerAnimations.cs
+++ Assets/Scripts/Controller/PlayerAnimations.cs
@@ -2,51 +2,41 @@ using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
-    [Header("Dependencies")]
-    [SerializeField] private SpriteRenderer _spriteRenderer;
-
     private PlayerController _player;
 
     private Animator _animator;
+
     private int _xSpeedHash;
     private int _ySpeedHash;
     private int _isGroundedHash;
-    private int _isSlidingHash;
-    private int _isDashingHash;
+    private int _jumpHash;
+    private int _isCrouchHash;
 
     private void Awake()
     {
         _player = GetComponent<PlayerController>();
-        _animator = GetComponent<Animator>();
+        _animator = GetComponentInChildren<Animator>();
 
         // set animations hashes
         _xSpeedHash = Animator.StringToHash("xSpeed");
         _ySpeedHash = Animator.StringToHash("ySpeed");
         _isGroundedHash = Animator.StringToHash("isGrounded");
-        _isSlidingHash = Animator.StringToHash("isSliding");
-        _isDashingHash = Animator.StringToHash("isDashing");
+        _jumpHash = Animator.StringToHash("jump");
+        _isCrouchHash = Animator.StringToHash("isCrouching");
     }
 
     private void Update()
     {
-        FlipSprite();
-
-        //if (_player.CanDash && _player.DashRequest)
-        //    _animator.SetTrigger(_isDashingHash);
+        if (_player.IsGrounded && _player.JumpRequest && _player.CurrentState!=PlayerStates.Crouching)
+            _animator.SetTrigger(_jumpHash);
     }
 
     private void LateUpdate()
     {
-        _animator.SetFloat(_xSpeedHash, Mathf.Abs(_player.Velocity.x));
+        _animator.SetFloat(_xSpeedHash, Mathf.Abs(new Vector2(_player.Velocity.x, _player.Velocity.z).magnitude));
         _animator.SetFloat(_ySpeedHash, _player.Velocity.y);
         _animator.SetBool(_isGroundedHash, _player.IsGrounded);
-        _animator.SetBool(_isSlidingHash, _player.IsWallTouching);
-        //_animator.SetBool(_isDashingHash, _player.CurrentState == PlayerStates.Dashing);
+        _animator.SetBool(_isCrouchHash, _player.CurrentState == PlayerStates.Crouching);
     }
 
-    private void FlipSprite()
-    {
-        if (!_spriteRenderer) return;
-        if (_player.MovementDirection.x == 0) return;
-    }
 }
